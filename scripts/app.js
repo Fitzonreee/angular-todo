d05eee67a4c2e980167f221b81f81e3cbb5e74f5
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-03-14 08:05:03 -0700

    ng-repeat an array of tasks from the controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
 angular.module("todoListApp", [])
   .controller('mainCtrl', function($scope){
 
+    $scope.todos = [
+      {"name": "finish this Angular app"},
+      {"name": "Run 2 times a week"},
+      {"name": "Don't Drink except happy hour"},
+      {"name": "finish Full Mean course"},
+      {"name": "Swim one day at CCC"}
+    ]
+
   });
