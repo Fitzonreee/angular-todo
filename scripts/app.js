7d84cb2a5d81b870871fc3aba4adfb4b17060723
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-03-17 07:55:55 -0700

    Added a service that allows you to delete a To Do

--- scripts/app.js
+++ scripts/app.js
@@ -4,9 +4,14 @@ angular.module("todoListApp", [])
     $scope.helloConsole = dataService.helloConsole;
 
     dataService.getTodos(function(response) {
-      console.log(response.data);
+      // console.log(response.data);
       $scope.todos = response.data;
     });
+
+    $scope.deleteTodo = function(todo, $index) {
+      dataService.deleteTodo(todo);
+      $scope.todos.splice($index, 1);
+    };
 
   })
   .service('dataService', function($http) {
@@ -17,5 +22,13 @@ angular.module("todoListApp", [])
     this.getTodos = function(callback) {
       $http.get('mock/todos.json')
         .then(callback)
+    }
+
+    this.deleteTodo = function(todo) {
+      console.log("This " + todo.name + " is deleted!");
+    }
+
+    this.saveTodo = function(todo) {
+      console.log("This " + todo.name + " is deleted!");
     }
 });
